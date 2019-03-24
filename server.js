025183149dd5e1a1e98c3f228114ff21d08ef16d
ropali <ropali68@gmail.com>
2019-03-24 11:43:09 +0530

    first deployment -v 1.0.0

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use("/", apiRoute);
 
 // Serve the static assets if in the prodcution 
 if(process.env.NODE_ENV === "production" ) {
+  app.use(express.static(path.resolve(__dirname, 'client', 'build', 'static')))
   // Set static folder
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
